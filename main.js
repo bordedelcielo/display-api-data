f06c9b7a418fa803d1847f1907351e05b13bf3a1
bordedelcielo <christopher.thrutchley@gmail.com>
2022-07-29 14:18:43 -0500

    re-written using HTML template

--- main.js
+++ main.js
@@ -11,74 +11,34 @@ function apiCall(year, month) {
 }
 
 function createTable(data) {
-    let tableDiv = document.getElementById('div1')
-    tableDiv.innerHTML = ''
-
-    const newTable = document.createElement('table')
-    newTable.setAttribute('id', 'resultsTable')
-    tableDiv.append(newTable)
-    // document.body.insertBefore(newTable, document.getElementById('div1'))
 
     for (let row of data.MRData.StandingsTable.StandingsLists[0].DriverStandings) {
-        newRow = document.createElement('tr')
         let position = row.position
         let fullName = row.Driver.givenName + ' ' + row.Driver.familyName
         let url = row.Driver.url
         let nation = row.Driver.nationality
-        let theArr = [position, fullName, nation]
 
-        for (let item of theArr) {
-            newField = document.createElement('td')
-            newRowContent = document.createTextNode(item)
-            newField.append(newRowContent)
-            newRow.append(newField)
-        }
+        let clone = myTemplate.content.cloneNode(true);
+        let td = clone.querySelectorAll('td');
+        td[0].textContent = position
+        td[1].textContent = fullName
+        td[2].textContent = nation
 
-        newTable.append(newRow)
+        tableBody.appendChild(clone);
     }
     }
-
-function addElement () {
-    const newDiv = document.createElement('div');
-    const newContent = document.createTextNode('General Kenobi, you are a bold one.')
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById('div1');
-    document.body.insertBefore(newDiv, currentDiv);
-}
-
-function removeElement() {
-    let myElement = document.getElementsByTagName('div')
-    console.log(myElement[0])
-    myElement[0].remove()
-}
-
-function clearBody() {
-    document.body.innerHTML = ''
-    // document.body.innerHTML = '<div id="div2"></div>'
-}
 
 const myForm = document.getElementById('form')
 
 myForm.addEventListener('submit', (event) => {
     event.preventDefault()
+    tableBody.innerHTML = ''
     const myForm = document.getElementById('form')
     formData = new FormData(myForm)
-    console.log(formData)
     console.log(event.target)
-    console.log('submitted')
     let myList = []
     for (const [key, value] of formData) {
-        console.log(key)
-        console.log(value)
         myList.push(value)
     }
     apiCall(myList[0], myList[1])
 });
-
-// onsubmit = (event) => {
-//     event.preventDefault()
-//     formData = new FormData(myForm)
-//     console.log('submission triggered')
-//     console.log(myForm)
-//     console.log(formData)
-// };
